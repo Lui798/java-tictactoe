4ef92a6d1e1b9d5eeacc005c240c10efac3f1b4e
Lui798 <42649201+Lui798@users.noreply.github.com>
2019-04-30 14:40:18 -0500

    work on file class

--- src/lui798/tictactoe/File.java
+++ src/lui798/tictactoe/File.java
@@ -1,17 +1,75 @@
 package lui798.tictactoe;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class File {
-    private FileSystem fs = FileSystems.getDefault();
-    //private String cd = Paths.get("").toAbsolutePath().toString();
-    private Path file = Paths.get("scores.txt");
+    private Path file;
+    private final int TIE_INDEX = 2;
+    private final int LOSS_INDEX = 1;
+    private final int WIN_INDEX = 0;
 
-    public void writeScore(String input) {
-        List<String> line = Arrays.asList(input);
-        Files.write(file, line, Charset.forName("UTF-8"));
+    public File(String file) {
+        this.file = Paths.get(file);
+        List<String> initLines = new ArrayList<String>();
+        writeFile(this.file, L);
+        addScore(0, LOSS_INDEX);
+        addScore(0, TIE_INDEX);
+    }
+
+    public void addWin() {
+        int score = getScore(WIN_INDEX);
+        score++;
+        addScore(score, WIN_INDEX);
+    }
+
+    public void addLoss() {
+        int score = getScore(LOSS_INDEX);
+        score++;
+        addScore(score, LOSS_INDEX);
+    }
+
+    public void addTie() {
+        int score = getScore(TIE_INDEX);
+        score++;
+        addScore(score, TIE_INDEX);
+    }
+
+    public int getScore(int index) {
+        List<String> lines = readFile(file);
+        return parseInt(lines.get(index));
+    }
+
+    private void addScore(int input, int index) {
+        List<String> lines = readFile(file);
+        lines.set(index, parseInt(input));
+        writeFile(file, lines);
+    }
+
+    private List<String> readFile(Path file) {
+        List<String> lines = new ArrayList<String>();
+        try {
+            lines = Files.readAllLines(file);
+        }
+        catch (IOException e) { }
+        return lines;
+    }
+
+    private void writeFile(Path file, List<String> lines) {
+        try {
+            Files.write(file, lines, Charset.forName("UTF-8"));
+        }
+        catch (IOException e) { }
+    }
+
+    private int parseInt(String s) {
+        return Integer.parseInt(s);
+    }
+
+    private String parseInt(int i) {
+        return Integer.toString(i);
     }
 }
